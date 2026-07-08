75f81b09cd468cbd1ed92de635ffecdd4313d18d
skashyap08 <sakshikashyap9876@gmail.com>
2026-07-08 11:46:55 +0530

    updated index .js-2

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"));
 });
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "about.html"));
+});
+
+app.get("/services", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "services.html"));
+});
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "contact.html"));
+});
